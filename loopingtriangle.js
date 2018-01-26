28145cc392af0a33803944023859a61e90afc9ac
Makoa <32736507+santarini@users.noreply.github.com>
2018-01-25 16:39:55 -1000

    Update loopingtriangle.js

--- loopingtriangle.js
+++ loopingtriangle.js
@@ -1,11 +1,23 @@
+//PASSING STRING
+//For loop with Array.join
+for(i = 1; i <= 7; i++){
+  console.log(Array(i).join("#"));
+}
+
+//For loop with .repeat
+for(i = 1; i <= 7; i++){
+  console.log("#".repeat(i));
+}
+
+//PASSING VARIABLE
 //For loop with Array.join
 var str = "#";
 for(i = 1; i <= 7; i++){
-console.log(Array(i).join(str));
+  console.log(Array(i).join(str));
 }
 
 //For loop with .repeat
 var str = "#";
 for(i = 1; i <= 7; i++){
-console.log(str.repeat(i));
+  console.log(str.repeat(i));
 }
