33e789cd7044c501f61e346df78aead1ff84b4e7
Makoa <32736507+santarini@users.noreply.github.com>
2018-01-27 09:32:26 -1000

    Create countChar.js

---
+++ countChar.js
@@ -0,0 +1,13 @@
+function countChar(str, char){
+  var currentLetter = 0;
+  var counter = 0;
+  while (currentLetter <= str.length){
+  		if (str.charAt(currentLetter) == char){
+      		counter += 1;
+    		currentLetter += 1;
+        }else{
+    		currentLetter += 1;
+        }
+  	}
+  return counter
+}
