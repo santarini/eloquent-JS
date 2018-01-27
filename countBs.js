a80d92cd1658bb9738fb4985a7f54dc51abde5cb
Makoa <32736507+santarini@users.noreply.github.com>
2018-01-27 09:24:47 -1000

    Update countBs.js

--- countBs.js
+++ countBs.js
@@ -1,40 +1,13 @@
-
-function countBs(str){
-	var currentLetter = 0;
-	var counter = 0;
-	while (currentLetter <= str.length){
-  		if (str.charAt(currentLetter) == "B")
-      		counter += 1;
-  	}
-}
-
-console.log(countBs("BBBBC"));
-console.log("CL is ",currentLetter);
-// → 2
-//console.log(countChar("kakkerlak", "k"));
-// → 4
-
-
-/*
-while (currentLetter <= totalLength){
-  	if str.charAt(currentLetter) == "B"
-      count += 1;
-  	else
-      return;
-  }
-  console.log(count)
-
-///
-
 var currentLetter = 0;
-var count = 0 ;
-
+var counter = 0;
 function countBs(str){
-  for(currentLetter = 0; currentLetter < str.length; currentLetter++)
-    if (str.charAt(currentLetter)=="B");
-  		count += 1;
-  		return count;
-        
+  while (currentLetter <= str.length){
+  	if (str.charAt(currentLetter) == "B"){
+      		counter += 1;
+    		currentLetter += 1;
+        }else{
+    		currentLetter += 1;
+        }
+  }
+  return counter
 }
-
- */
