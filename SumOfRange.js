3a142f63ad3f053780ed9a2d1beb6428f737a7bd
Makoa <32736507+santarini@users.noreply.github.com>
2018-01-29 10:58:09 -1000

    Update SumOfRange.js

--- SumOfRange.js
+++ SumOfRange.js
@@ -8,3 +8,29 @@ function range(start, end){
     }  
 	return numList
 }
+
+//Sums the values in an array
+
+function sum(selArray){
+	var result = 0;
+  	var i = 0;
+  	while (i< selArray.length){
+    	result = result + selArray[i];
+      	i+=1;
+    }
+  return result;
+}
+
+//Creates an array from the start int to the finish int (inclusive)
+
+function range(start, end, step){
+	var numList = [];
+  	while(start<=end){
+    	numList.push(start);
+	if step == undefined;
+      	start += 1;
+	else
+	start += step;
+    }  
+	return numList
+}
