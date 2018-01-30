bf96e93d4afb3ada46a51ee28e0033d8c3098f23
Makoa <32736507+santarini@users.noreply.github.com>
2018-01-29 15:57:41 -1000

    Update ReversingArray.js

--- ReversingArray.js
+++ ReversingArray.js
@@ -1,1 +1,15 @@
+//////////////////
+
+function reverseArray(trgt){
+  var revArray = [];
+  var i = 0;
+  var j = trgt.length - 1;
+  while(i <= j){
+    revArray.push(trgt[j]);
+    j -=1;
+  }
+  return revArray;
+}
+
+///////////////
 
