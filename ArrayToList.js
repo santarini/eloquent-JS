a97147f8bcd76e9eb314df422445410e82fdb5a2
Makoa <32736507+santarini@users.noreply.github.com>
2018-01-30 15:20:13 -1000

    Update ArrayToList.js

--- ArrayToList.js
+++ ArrayToList.js
@@ -1,8 +1,8 @@
-///
-var arrayToList = function(arr) {
+var arrayToList = function(array) {
   var list = null;
-  for(i = arr.length - 1; i >= 0; i--) {
-    list = { value: arr[i], rest: list };
+  for(i = array.length - 1; i >= 0; i--) {
+    list = { value: array[i], rest: list };
   }
   return list;
 }
+
